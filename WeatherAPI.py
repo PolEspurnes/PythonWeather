698a0b5d157f7c2941d5858301c63a2308101eb5
PolEspurnes <polcentellesperxes@gmail.com>
2020-08-28 10:39:14 +0200

    API request and show results on screen DONE

--- WeatherAPI.py
+++ WeatherAPI.py
@@ -1,4 +1,5 @@
 import requests
+from CitiesDictionary import create_dictionary
 
 '''
 Objectives:
@@ -21,6 +22,41 @@ class WeatherAPI:
             key_file = open(key_filename, "r")
             self.key = key_file.readline()
             key_file.close()
+            self.cities = create_dictionary("city.list.json")
         except FileNotFoundError:
             print("File not found")
-        pass
+
+    def weather_request(self, city):
+        l = len(self.cities[city])
+        if l > 1:
+            for i in range(l):
+                print("  "+str(i+1)+". "+city+" - "+self.cities[city][i]["country"])
+            try:
+                option = int(input("Choose option: ")) - 1
+                id = self.cities[city][option]["id"]
+            except ValueError:
+                print("Invalid input")
+                return
+            except IndexError:
+                print("Not an option")
+                return
+        else:
+            id = self.cities[city][0]["id"]
+
+        url = "https://api.openweathermap.org/data/2.5/weather?id=" + str(id) + "&units=metric&appid=" + self.key
+        response = requests.get(url)
+        if response:
+            print('Request is successful.')
+            self.show_results(response.json(), city)
+        else:
+            print('Request returned an error.')
+
+
+    def show_results(self, response, city):
+        print("\n*---------- "+city+" "+response["sys"]["country"]+" ----------*")
+        weather = response["weather"][0]["main"]
+        print("\n* Weather conditions: "+weather)
+        temps = response["main"]
+        print("\n* Actual temperature: "+ str(temps["temp"]) +"ºC")
+        print("\n* Minimum temperature: " + str(temps["temp_min"]) + "ºC")
+        print("\n* Maximum temperature: " + str(temps["temp_max"]) + "ºC\n")
